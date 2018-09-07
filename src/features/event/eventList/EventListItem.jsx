2f723391adf609e3072df66da56e07ac5fd4799b
Vitor Boccio <vitorboccio@gmail.com>
2018-09-07 12:35:43 +1000

    json mock for events items

--- src/features/event/eventList/EventListItem.jsx
+++ src/features/event/eventList/EventListItem.jsx
@@ -4,36 +4,39 @@ import EventListAttendee from './EventListAttendee'
 
 class EventListItem extends Component {
   render() {
+    const {event} = this.props;
     return (
         <Segment.Group>
             <Segment>
-            <Item.Group>
-                <Item>
-                <Item.Image size="tiny" circular src="https://randomuser.me/api/portraits/women/42.jpg" />
-                <Item.Content>
-                    <Item.Header as="a">Event Title</Item.Header>
-                    <Item.Description>
-                    Hosted by <a>hosted by</a>
-                    </Item.Description>
-                </Item.Content>
-                </Item>
-            </Item.Group>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+                        <Item.Content>
+                            <Item.Header as="a">{event.title}</Item.Header>
+                            <Item.Description>
+                            Hosted by <a>{event.hostedBy}</a>
+                            </Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
             </Segment>
             <Segment>
-            <span>
-                <Icon name="clock" /> date |
-                <Icon name="marker" /> time
-            </span>
+                <span>
+                    <Icon name="clock" /> {event.date}
+                    <Icon name="marker" /> {event.venue}
+                </span>
             </Segment>
             <Segment secondary>
-            <List horizontal>
-                <EventListAttendee />
-                <EventListAttendee />
-                <EventListAttendee />
-            </List>
+                <List horizontal>
+                {event.attendees.map((attendee) => (
+                    <EventListAttendee key={attendee.id} attendee={attendee} />
+                ))}    
+                    
+                </List>
             </Segment>
             <Segment clearing>
-            <Button as="a" color="teal" floated="right" content="View" />
+                <span>{event.description}</span>
+                <Button as="a" color="teal" floated="right" content="View" />
             </Segment>
         </Segment.Group>
     )

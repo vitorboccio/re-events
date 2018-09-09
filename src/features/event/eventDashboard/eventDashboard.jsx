06bf66a1434cd9c4d0a4b0f21e23c6153838c269
Vitor Boccio <vitorboccio@gmail.com>
2018-09-09 18:41:58 +1000

    event form method

--- src/features/event/eventDashboard/eventDashboard.jsx
+++ src/features/event/eventDashboard/eventDashboard.jsx
@@ -63,7 +63,22 @@ class EventDashboard extends Component {
             events: eventsDashboard,
             isOpen: false
         }
+        this.handleFormOpen = this.handleFormOpen.bind(this)
+        this.handleCancel = this.handleCancel.bind(this)
     }
+
+    handleFormOpen() {
+        this.setState({
+            isOpen: true
+        })
+    }
+
+    handleCancel() {
+        this.setState({
+            isOpen: false
+        })
+    }
+
     render() {
         return (
             <Grid>
@@ -71,8 +86,8 @@ class EventDashboard extends Component {
                     <EventList events={this.state.events}/>   
                 </Grid.Column>
                 <Grid.Column width={6}>
-                    <Button positive content="Create Event"/>
-                    {this.state.isOpen && <EventForm/>}
+                    <Button onClick={this.handleFormOpen} positive content="Create Event"/>
+                    {this.state.isOpen && <EventForm handleCancel={this.handleCancel}/>}
                 </Grid.Column>
             </Grid>
         )

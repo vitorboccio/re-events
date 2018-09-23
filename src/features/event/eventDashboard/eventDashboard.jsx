50d5f105478496089e24ee69e05633a77dcb0ae6
Vitor Boccio <vitorboccio@gmail.com>
2018-09-23 21:16:13 +1000

    connecting component with redux store

--- src/features/event/eventDashboard/eventDashboard.jsx
+++ src/features/event/eventDashboard/eventDashboard.jsx
@@ -1,64 +1,16 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Grid, Button } from 'semantic-ui-react'
 import cuid from 'cuid';
 import EventList from '../eventList/EventList'
 import EventForm from '../eventForm/EventForm'
 
-const eventsDashboard = [
-  {
-    'id': '1',
-    'title': 'Trip to Tower of London',
-    'date': '2018-03-27',
-    'category': 'culture',
-    'description':
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    'city': 'London, UK',
-    'venue': "Tower of London, St Katharine's & Wapping, London",
-    'hostedBy': 'Bob',
-    'hostPhotoURL': 'https://randomuser.me/api/portraits/men/20.jpg',
-    'attendees': [
-      {
-        'id': 'a',
-        'name': 'Bob',
-        'photoURL': 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        'id': 'b',
-        'name': 'Tom',
-        'photoURL': 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    'id': '2',
-    'title': 'Trip to Punch and Judy Pub',
-    'date': '2018-03-28',
-    'category': 'drinks',
-    'description':
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    'city': 'London, UK',
-    'venue': 'Punch & Judy, Henrietta Street, London, UK',
-    'hostedBy': 'Tom',
-    'hostPhotoURL': 'https://randomuser.me/api/portraits/men/22.jpg',
-    'attendees': [
-      {
-        'id': 'b',
-        'name': 'Tom',
-        'photoURL': 'https://randomuser.me/api/portraits/men/22.jpg'
-      },
-      {
-        'id': 'a',
-        'name': 'Bob',
-        'photoURL': 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-]
-
+const mapState = (state) => ({
+    events: state.events
+})
 
 class EventDashboard extends Component {
     state = {
-        events: eventsDashboard,
         isOpen: false,
         selectedEvent: null
     }
@@ -118,10 +70,11 @@ class EventDashboard extends Component {
 
     render() {
         const {selectedEvent} = this.state;
+        const {events} = this.props; 
         return (
             <Grid>
                 <Grid.Column width={10}>
-                    <EventList deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={this.state.events}/>   
+                    <EventList deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={events}/>   
                 </Grid.Column>
                 <Grid.Column width={6}>
                     <Button onClick={this.handleFormOpen} positive content="Create Event"/>
@@ -135,4 +88,4 @@ class EventDashboard extends Component {
     }
 } 
 
-export default EventDashboard;
+export default connect(mapState)(EventDashboard);
